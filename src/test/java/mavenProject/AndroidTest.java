f39680b70f1bfb0516431f608020b36d8e6ee8cb
Angshuman <angshban@gmail.com>
2021-03-12 08:50:50 +0530

    commit in the new branch

---
+++ src/test/java/mavenProject/AndroidTest.java
@@ -0,0 +1,12 @@
+package mavenProject;
+
+import org.testng.annotations.Test;
+
+public class AndroidTest {
+	
+	@Test
+	public void testAppium() {
+		System.out.println("Added android test in the develop branch");
+	}
+
+}
